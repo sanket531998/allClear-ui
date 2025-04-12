da8c2269cc13a5611ece86c1b1a82251b8e1aba6
sanket_35 <81851656+sanket531998@users.noreply.github.com>
2025-04-12 21:59:13 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import SignIn from "./pages/SignIn";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import Admin from "./pages/Admin";
+import EditArticle from "./components/EditArticle";
+import Article from "./pages/Article";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/category/:category" element={<Index />} />
+          <Route path="/category/:category/:subcategory" element={<Index />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/edit/:id" element={<EditArticle />} />
+          <Route path="/article/:id" element={<Article />}/>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
